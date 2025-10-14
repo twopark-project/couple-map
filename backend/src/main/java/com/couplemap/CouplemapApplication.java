2e5a4c736148500fe0e22f7772e6fea73b6f6ea3
Minkyu Park <rb6609@naver.com>
2025-10-15 01:11:25 +0900

    feat: BaseEntity 및 JPA Auditing 설정 추가

--- backend/src/main/java/com/couplemap/CouplemapApplication.java
+++ backend/src/main/java/com/couplemap/CouplemapApplication.java
@@ -2,8 +2,10 @@ package com.couplemap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CouplemapApplication {
 
 	public static void main(String[] args) {
