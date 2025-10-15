eddb9fc40e5898a2a01b6c5380135205c89ae149
Minkyu Park <rb6609@naver.com>
2025-10-15 12:21:33 +0900

    feat: @Repository 어노테이션 추가

--- backend/src/main/java/com/couplemap/friend/repository/FriendshipRepository.java
+++ backend/src/main/java/com/couplemap/friend/repository/FriendshipRepository.java
@@ -2,6 +2,8 @@ package com.couplemap.friend.repository;
 
 import com.couplemap.friend.domain.Friendship;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
 }
