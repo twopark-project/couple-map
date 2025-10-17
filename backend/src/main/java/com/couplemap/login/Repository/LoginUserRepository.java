2acbe4aafd7ce3d5d74dfe7d75eebd3c25a18c14
sungbeen park <psb1105a@gmail.com>
2025-10-17 16:35:40 +0900

    fix: 카카오 공식 client-authentication-method 변경 및 user 변경사항 삭제

--- backend/src/main/java/com/couplemap/login/Repository/LoginUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.couplemap.login.Repository;
-
-import com.couplemap.login.Entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginUserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByUsername(String username);
-}
