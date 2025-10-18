365f1921e8e9fd03bf5cb7a55bb15c30581dede6
Minkyu Park <rb6609@naver.com>
2025-10-18 18:53:13 +0900

    feat : UserDTO 필드 변경 - name -> oauthId로 이름 변경 - userId 추가 - 빌더를 통한 생성자 추가

--- backend/src/main/java/com/couplemap/login/dto/UserDTO.java
+++ backend/src/main/java/com/couplemap/login/dto/UserDTO.java
@@ -1,13 +1,24 @@
 package com.couplemap.login.dto;
 
 import com.couplemap.user.domain.UserRole;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserDTO {
+    private Long userId;
     private UserRole role;
-    private String name;
+    private String oauthId;
     private String username;
+
+    @Builder
+    public UserDTO(Long userId, UserRole role, String oauthId, String username) {
+        this.userId = userId;
+        this.role = role;
+        this.oauthId = oauthId;
+        this.username = username;
+    }
+
 }
