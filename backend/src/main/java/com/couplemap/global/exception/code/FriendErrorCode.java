5e5d77179e1e733695dd788adabaa6fac39af399
Minkyu Park <rb6609@naver.com>
2025-10-18 18:52:11 +0900

    feat : Friend관련 에러코드 추가

--- backend/src/main/java/com/couplemap/global/exception/code/FriendErrorCode.java
+++ backend/src/main/java/com/couplemap/global/exception/code/FriendErrorCode.java
@@ -4,15 +4,17 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Getter
 @RequiredArgsConstructor
 public enum FriendErrorCode implements ErrorCode {
 
     FRIEND_ALREADY_EXISTS(CONFLICT, "이미 친구인 사용자입니다."),
-    FRIEND_REQUEST_NOT_FOUND(NOT_FOUND, "친구 요청 코드가 존재하지 않습니다.");
+    FRIEND_PENDING_EXISTS(CONFLICT, "이미 친구요청을 보낸 사용자입니다."),
+    INVALID_FRIEND_CODE(BAD_REQUEST, "친구 코드가 일치하지 않습니다."),
+    CANNOT_FRIEND_YOURSELF(BAD_REQUEST, "자기 자신에게 친구 요청을 보낼 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
