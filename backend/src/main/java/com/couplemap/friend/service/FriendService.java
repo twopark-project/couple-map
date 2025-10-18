cc0fb861686e82fd62bd28eb96fc2cdccb21af15
Minkyu Park <rb6609@naver.com>
2025-10-18 18:57:58 +0900

    refactor : sendFriendRequest에 requesterId 인자 추가

--- backend/src/main/java/com/couplemap/friend/service/FriendService.java
+++ backend/src/main/java/com/couplemap/friend/service/FriendService.java
@@ -4,5 +4,5 @@ import com.couplemap.friend.dto.FriendRequestResponseDto;
 import com.couplemap.friend.dto.SendFriendRequestDto;
 
 public interface FriendService {
-    FriendRequestResponseDto sendFriendRequest(SendFriendRequestDto dto);
+    FriendRequestResponseDto sendFriendRequest(SendFriendRequestDto dto, Long requesterId);
 }
