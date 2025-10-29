66a059b827a15fceacfc8ffacb2d17e4a3974e31
Minkyu Park <rb6609@naver.com>
2025-10-29 18:32:57 +0900

    refactor: Public 제어자 제거

--- backend/src/main/java/com/couplemap/global/s3/S3Service.java
+++ backend/src/main/java/com/couplemap/global/s3/S3Service.java
@@ -3,6 +3,6 @@ package com.couplemap.global.s3;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface S3Service {
-    public S3UploadDto uploadImageFile(MultipartFile file);
-    public void deleteFile(String deleteKey);
+    S3UploadDto uploadImageFile(MultipartFile file);
+    void deleteFile(String deleteKey);
 }
