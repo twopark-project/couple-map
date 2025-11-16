672ec3c0f5fcfb584ae069a4fa920c346afa61b4
Minkyu Park <rb6609@naver.com>
2025-11-16 16:19:04 +0900

    docs: Swagger/OpenAPI 설정 추가

---
+++ backend/src/main/java/com/couplemap/global/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.couplemap.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+
+        SecurityScheme accessTokenScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("Bearer")
+                .bearerFormat("JWT")
+                .name("Bearer Authentication");
+
+        SecurityScheme refreshTokenScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("Bearer")
+                .bearerFormat("JWT")
+                .name("Refresh Token (Bearer)");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CoupleMap API")
+                        .version("1.0")
+                        .description("커플맵 API 문서"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", accessTokenScheme)
+                        .addSecuritySchemes("Refresh Token (Bearer)", refreshTokenScheme)
+                )
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication"));
+    }
+}
