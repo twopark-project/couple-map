f118786e93a292cab11eefbc231e5ca4be96995f
Minkyu Park <rb6609@naver.com>
2025-11-24 18:57:03 +0900

    feat: 닉네임 관련 에러 코드 추가 #46

--- backend/src/main/java/com/couplemap/global/exception/code/UserErrorCode.java
+++ backend/src/main/java/com/couplemap/global/exception/code/UserErrorCode.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
-    USER_NOT_FOUND(NOT_FOUND, "사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    DUPLICATE_NICKNAME(CONFLICT, "이미 사용 중인 닉네임입니다."),
+    INVALID_NICKNAME_FORMAT(BAD_REQUEST, "닉네임 형식이 올바르지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
